812a462af3d7c4af4ed4d9aa53bf24853ec6dc16
Philip <126236947+stuckinsnow@users.noreply.github.com>
2024-05-02 14:36:50 +0100

    fix styling.

--- src/app/_components/PaginationButton/index.tsx
+++ src/app/_components/PaginationButton/index.tsx
@@ -16,34 +16,43 @@ const PaginationButton: React.FC<{ searchParams: string, posts: any, paramCat: n
     const shouldShowNextPage = hasNextPage || truePage < totalPages;
 
     return (
-        <div className='flex h-7 gap-4 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary bg-muted font-medium text-primary'>
-            {shouldShowPrevPage && (
-                <>
-                    <Link className='order-first' href={`/posts/?page=${previousPage}&category=${paramCat}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
-                            <path fillRule='evenodd' d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
-                        </svg>
-                    </Link>
-                    {truePage > 2 && <Link href={`/posts/?page=1&category=${paramCat}`}>1</Link>}
-                    {truePage > 3 && <span>...</span>}
-                    <Link href={`/posts/?page=${previousPage}&category=${paramCat}`}>{previousPage}</Link>
-                </>
-            )}
-            <span className='bg-red-200 p-2'>
-                {truePage}
-            </span>
-            {shouldShowNextPage && (
-                <>
-                    {truePage <= totalPages - 2 && <Link href={`/posts/?page=${nextPage}&category=${paramCat}`}>{nextPage}</Link>}
-                    {truePage < totalPages - 2 && <span>...</span>}
-                    {truePage < totalPages && <Link href={`/posts/?page=${totalPages}&category=${paramCat}`}>{totalPages}</Link>}
-                    <Link href={`/posts/?page=${nextPage}&category=${paramCat}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-                            <path fillRule='evenodd' d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                        </svg>
-                    </Link>
-                </>
-            )}
+
+        <div className={`wrapper ${truePage === totalPages ? 'pr-16' : ''} ${!shouldShowPrevPage ? 'pl-20' : ''}`}>
+
+
+            <div className={`flex h-7 gap-4 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary bg-muted font-medium text-primary ${truePage === 1 ? 'mx-28' : 'mx-48'}`}>
+
+                {shouldShowPrevPage && (
+                    <>
+                        <Link className='order-first' href={`/posts/?page=${previousPage}&category=${paramCat}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+                                <path fillRule='evenodd' d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+                            </svg>
+                        </Link>
+                        {truePage > 2 && <Link href={`/posts/?page=1&category=${paramCat}`}>1</Link>}
+                        {truePage > 3 && <span>...</span>}
+                        <Link href={`/posts/?page=${previousPage}&category=${paramCat}`}>{previousPage}</Link>
+                    </>
+                )}
+
+                <span className='bg-red-200 p-2'>
+                    {truePage}
+                </span>
+
+
+                {shouldShowNextPage && (
+                    <>
+                        {truePage <= totalPages - 2 && <Link href={`/posts/?page=${nextPage}&category=${paramCat}`}>{nextPage}</Link>}
+                        {truePage < totalPages - 2 && <span>...</span>}
+                        {truePage < totalPages && <Link href={`/posts/?page=${totalPages}&category=${paramCat}`}>{totalPages}</Link>}
+                        <Link href={`/posts/?page=${nextPage}&category=${paramCat}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fillRule='evenodd' d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                            </svg>
+                        </Link>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
--- src/app/_components/SearchAndFilter/Categories/index.tsx
+++ src/app/_components/SearchAndFilter/Categories/index.tsx
@@ -11,7 +11,7 @@ const Categories: React.FC<{ newCats: string[] }> = ({ newCats }) => {
                 const theLink = <div key={cat.id} className="categories-button"><Link key={cat.id} href={process.env.NEXT_PUBLIC_SERVER_URL + '/posts/' + '?page=1' + '&category=' + cat.id}>{cat.title}</Link></div>
                 return theLink;
             })}
-            <div className="categories-button"><Link href={process.env.NEXT_PUBLIC_SERVER_URL + '/posts/'}>All</Link></div>
+            <div className="categories-button"><Link href={process.env.NEXT_PUBLIC_SERVER_URL + '/posts/?page=0&category=0'}>All</Link></div>
         </React.Fragment>
     )
 }
