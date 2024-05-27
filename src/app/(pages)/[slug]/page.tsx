6dfe44fc98a19e6904f61a196edff59cb41d642e
Philip <126236947+stuckinsnow@users.noreply.github.com>
2024-05-27 13:43:30 +0100

    chore: update readme.

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head';
 import { getDoc } from '../../_api/getDoc';
-// import { PAGE } from '../../_graphql/pages';
 import Image from 'next/image';
-import { Page as PageType, Media } from '../../../pl-types';
+import { Page as PageType } from '../../../pl-types';
 
 import './page.scss';
 
@@ -12,13 +11,13 @@ import ContentAndMediaBlock from '../../_blocks/ContentAndMediaBlock';
 export const dynamic = 'force-dynamic';
 
 // things to fix include type and css classnames
+// this page is unimportant because it's not currently used but I don't want to remove it yet
 
 export type MediaBlock = Extract<PageType['layout'][number], { blockType: 'mediaBlock' }>;
 
 export default async function Page({ params }: { params: { slug: string } }) {
     try {
         const data = await getDoc("pages", params.slug);
-
 
         const dataType = data as any;
         const page = dataType?.Pages?.docs[0] || null;
@@ -68,33 +67,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
                     <div className='ab1'><h2>main hero richText</h2></div>
                     <RichText content={page.hero && page.hero.media && page.hero.richText} />
-
                     <div className='ab2'><h2>main content richText</h2></div>
-
-                    {/* {page?.layout?.map((layout: Extract<PageType['layout'][number], { blockType: 'content' | 'mediaBlock' }>, layoutIndex: number) => {
-                        if (layout.blockType === 'content') {
-                            return layout.columns?.map((column, columnIndex) => (
-                                column.richText && (
-                                    <div className='ab3' key={`${layoutIndex}-${columnIndex}`}>
-                                        <h3>content richText</h3>
-                                        <RichText key={`${layoutIndex}-${columnIndex}`} content={column.richText} />
-                                    </div>
-                                )
-                            ));
-                        } else if (layout.blockType === 'mediaBlock') {
-                            const layoutMedia = layout.media as Media;
-                            return (
-                                <div className='ab4' key={layoutIndex}>
-                                    <h3>content mediaBlock</h3>
-                                    <img className="mediablock-image" src={`${process.env.NEXT_PUBLIC_PAYLOAD_URL}${layoutMedia.url}`} />
-                                </div>
-                            );
-
-                        }
-                        return null;
-                    })} */}
-
-
                     <ContentAndMediaBlock rData={dataType.Pages.docs[0]} />
 
 
