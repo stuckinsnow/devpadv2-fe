a6437e5d9bd98de61262ce0193e3c33b2cb88462
Philip <126236947+stuckinsnow@users.noreply.github.com>
2024-05-30 11:53:00 +0100

    chore: fix hydration error.

--- src/app/(pages)/help/page.tsx
+++ src/app/(pages)/help/page.tsx
@@ -1,70 +1,96 @@
 import React from 'react';
 import { getDocs } from '../../_api/getDocs';
+import { codeToHtml } from 'shiki';
+import '../../_css/globals.scss';
 
+
+async function renderCode(content: string, lang = 'javascript', theme = 'catppuccin-macchiato') {
+    const codeRegex = /<pre><code.*?>([\s\S]*?)<\/code><\/pre>/g;
+    const matches = content.split(codeRegex);
+    const renderedContent = [];
+
+    for (let i = 0; i < matches.length; i++) {
+        if (i % 2 === 0) {
+            renderedContent.push(matches[i]);
+        } else {
+
+            const codeBlock = `<pre><code class="${lang}">${matches[i]}</code></pre>`;
+            renderedContent.push(codeBlock);
+
+            // const html = await codeToHtml(matches[i], { lang, theme });
+            // renderedContent.push(html);
+        }
+    }
+
+    return renderedContent;
+}
+
+export const dynamic = 'force-dynamic';
 
 export default async function HelpPage() {
-
-    const discordThreadData = await getDocs("discordthreads", undefined, undefined, undefined);
+    const discordThreadData = await getDocs('discordthreads', undefined, undefined, undefined);
     const threads = discordThreadData?.DiscordCommunities.docs;
 
-    // console.log('bebop', threads[0].discordCommunityJSON.messages);
+    const renderedMessages: { [key: string]: any[] } = {};
 
-    // https://discord.com/api/v10/channels/1244807389660844093/threads/archived/public // get all archived threads
-    // https://discord.com/api/v10/channels/1244807389660844093/messages // get all messages of channel
-    // https://discord.com/api/v10/channels/1244807614924066826/messages // get all messages in thread id
+    if (Array.isArray(threads)) {
+        for (let thread of threads) {
+            if (Array.isArray(thread.discordCommunityJSON.messages)) {
+                const renderPromises = thread.discordCommunityJSON.messages.map(async (json: any) => {
+                    const rendered = await renderCode(json.content);
+                    return {
+                        renderedContent: rendered,
+                        ...json,
+                    };
+                });
+                renderedMessages[thread.id] = await Promise.all(renderPromises);
+            }
+        }
+    }
 
 
     return (
         <React.Fragment>
             <h1>Help</h1>
+            <div className="content">
+                <div className="rounded-lg flex flex-col mx-auto w-full max-w-screen-lg">
+                    {Array.isArray(threads) ? threads.map((thread: any) => {
+                        const nameID = thread.discordCommunityJSON.intro.authorID.toString().slice(-4);
+                        const globalNameFixed = thread.discordCommunityJSON.intro.globalName + nameID;
+                        const rawContent = thread.discordCommunityJSON.intro.content;
+                        const paragraphs = rawContent.split('<br>').map((paragraph: string, index: number) => (
+                            <p key={index} className="mt-2">{paragraph}</p>
+                        ));
 
-            <div className='content'>
-
-                <div className='postcards rounded-lg flex flex-wrap justify-center mx-auto w-full'>
-
-                    <div className='postcards rounded-lg flex flex-wrap justify-center mx-auto w-full'>
-
-                        {/* <PostCards posts={postsLowImpact} /> */}
-
-                        {threads && Array.isArray(threads) ? threads.map((thread: any) => {
-
-                            // console.log('content', thread.discordCommunityJSON.intro.content);
-
-                            return (
-                                <article key={thread.id} className={Array.isArray(thread.title) ? thread.title.join(' ') + ' card w-full md:w-5/12 md:m-2 lg:w-9/12 flex flex-col lg:flex-row p-3 mt-4 rounded-md transition-all duration-75 nice-shadow box-border' : ''}>
-
-
-                                    <div className='lg:h-40 p-2 lg:p-0 box-border flex flex-col justify-around items-start text-left lg:ml-3 w-full h-full'><div key={thread.discordID}>
-                                        <p>title {thread.title}</p>
-                                        <div>slug {thread.slug}</div>
-                                        <div>discord id {thread.discordID}</div>
-                                        <div>content {thread.discordCommunityJSON.intro.content}</div>
+                        return (
+                            <article key={thread.id} className={Array.isArray(thread.title) ? thread.title.join(' ') + 'aa' : 'mt-4'}>
+                                <div className="p-2 border-solid border-2 border-slate-800">
+                                    <div key={thread.discordID}>
+                                        <div>
+                                            name
+                                            <span className="capitalise"> {globalNameFixed}</span>
+                                        </div>
+                                        <p>Title: {thread.title}</p>
+                                        <div>Slug: {thread.slug}</div>
+                                        <div>content {paragraphs} </div>
+                                        <div>message count {thread.discordCommunityJSON.messageCount}</div>
                                     </div>
-
-                                        {/* {Array.isArray(thread.discordCommunityJSON.messages) ? thread.discordCommunityJSON.messages.map((json) => {
-                                            return (
-                                                <div key={json.id} className='flex flex-col message-box bg-slate-800 text-fuchsia-400'>
-                                                    <p>Content: {json.content}</p>
-                                                    <p>Author ID: {json.authorID}</p>
-                                                    <p>Author Name: {json.authorName}</p>
-                                                    <p>Created At: {new Date(json.createdAtDate).toLocaleString()}</p>
-                                                    <div>Attachments: {json.fileAttachments.length > 0 ? json.fileAttachments.join(', ') : 'None'}</div>
-                                                </div>
-                                            );
-                                        }) : null} */}
-
+                                    <div>
+                                        {renderedMessages[thread.id] ? renderedMessages[thread.id].map((json, i) => (
+                                            <div key={i} className="flex flex-col message-box bg-slate-800 text-fuchsia-400 mt-4">
+                                                <div dangerouslySetInnerHTML={{ __html: json.renderedContent.join('') }} />
+                                                <p>Author Name: {json.globalName}{json.authorID.toString().slice(-4)}</p>
+                                                <p>Created At: {new Date(json.createdAtDate).toLocaleString()}</p>
+                                                <div>Attachments: {json.fileAttachments.length > 0 ? json.fileAttachments.join(', ') : 'None'}</div>
+                                            </div>
+                                        )) : null}
                                     </div>
-
-                                </article>
-                            );
-                        }) : null}
-
-
-                    </div>
+                                </div>
+                            </article>
+                        );
+                    }) : null}
                 </div>
             </div>
-
         </React.Fragment>
     );
 }
-
